f197708973f5f6be2dfad8b22ad64cba714dabc3
Chris <chris@steema.com>
2018-05-17 10:57:33 +0200

    ToDataTable extension moved to ASP.NET Core demo

--- AppNetCore/Controllers/HomeController.cs
+++ AppNetCore/Controllers/HomeController.cs
@@ -255,4 +255,29 @@ namespace TeeGridAspNetCore.Controllers
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
+
+	public static class Extentions
+	{
+		public static DataTable ToDataTable<T>(this IList<T> data)
+		{
+			PropertyDescriptorCollection props =
+			TypeDescriptor.GetProperties(typeof(T));
+			DataTable table = new DataTable();
+			for (int i = 0; i < props.Count; i++)
+			{
+				PropertyDescriptor prop = props[i];
+				table.Columns.Add(prop.Name, prop.PropertyType);
+			}
+			object[] values = new object[props.Count];
+			foreach (T item in data)
+			{
+				for (int i = 0; i < values.Length; i++)
+				{
+					values[i] = props[i].GetValue(item);
+				}
+				table.Rows.Add(values);
+			}
+			return table;
+		}
+	}
 }
