b02976238b6416f75742f6496ffaf41b91ecd1b2
Pep <pep@steema.com>
2018-07-04 15:48:00 +0200

    Added TeeGridFeatures launcher demo. Remember. All the demos accesible from this one should be downloaded and compiled before to run this one.

---
+++ TeeGridFeatures/launcher/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace launcher
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button12 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(552, 63);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(284, 71);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Cell editing";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(552, 161);
+			this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(284, 71);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Custom Sorting";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(552, 259);
+			this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(284, 71);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Client Dataset";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(552, 357);
+			this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(284, 71);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Master Detail";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.ForeColor = System.Drawing.Color.White;
+			this.button5.Location = new System.Drawing.Point(552, 553);
+			this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(284, 71);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "Header and Footer";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.ForeColor = System.Drawing.Color.White;
+			this.button6.Location = new System.Drawing.Point(871, 455);
+			this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(284, 71);
+			this.button6.TabIndex = 9;
+			this.button6.Text = "Array data";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.ForeColor = System.Drawing.Color.White;
+			this.button7.Location = new System.Drawing.Point(871, 357);
+			this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(284, 71);
+			this.button7.TabIndex = 8;
+			this.button7.Text = "Ticker";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.ForeColor = System.Drawing.Color.White;
+			this.button8.Location = new System.Drawing.Point(871, 259);
+			this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(284, 71);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "Static Charts";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button9.ForeColor = System.Drawing.Color.White;
+			this.button9.Location = new System.Drawing.Point(871, 161);
+			this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(284, 71);
+			this.button9.TabIndex = 6;
+			this.button9.Text = "Row Heights";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button10.ForeColor = System.Drawing.Color.White;
+			this.button10.Location = new System.Drawing.Point(871, 63);
+			this.button10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(284, 71);
+			this.button10.TabIndex = 5;
+			this.button10.Text = "Locked Columns";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button11.ForeColor = System.Drawing.Color.White;
+			this.button11.Location = new System.Drawing.Point(871, 553);
+			this.button11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(284, 71);
+			this.button11.TabIndex = 10;
+			this.button11.Text = "Virtual Mode";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(47, 345);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(315, 31);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Grid component for .NET";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::launcher.Properties.Resources.teegrid300x83transp;
+			this.pictureBox2.Location = new System.Drawing.Point(40, 116);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(421, 105);
+			this.pictureBox2.TabIndex = 13;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::launcher.Properties.Resources.logo_steema_v3;
+			this.pictureBox1.Location = new System.Drawing.Point(53, 453);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(227, 135);
+			this.pictureBox1.TabIndex = 12;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button12
+			// 
+			this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button12.ForeColor = System.Drawing.Color.White;
+			this.button12.Location = new System.Drawing.Point(552, 455);
+			this.button12.Margin = new System.Windows.Forms.Padding(4);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(284, 71);
+			this.button12.TabIndex = 14;
+			this.button12.Text = "Themes";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(89)))), ((int)(((byte)(141)))));
+			this.ClientSize = new System.Drawing.Size(1255, 659);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Steema TeeGrid Examples";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button button12;
+	}
+}
+
---
+++ TeeGridFeatures/launcher/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace launcher
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		string folder = Application.StartupPath +  @"\..\..\..\..\";
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Cell Editors\bin\Debug\TeeGrid_Cell_Editors.exe");
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Sorting\Custom Sorting\bin\Debug\TeeGrid_Custom_Sorting.exe");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Database\ClientDataSet\bin\Debug\ClientDataSet.exe");
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Database\Master Detail\bin\Debug\TeeGrid_WinForm_Master_Detail.exe");
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Header Footer\bin\Debug\TeeGrid_Header_Footer.exe");
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Locked Columns\bin\Debug\TeeGrid_Locked_Columns.exe");
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Row Heights\bin\Debug\TeeGrid_WinForm_Row_Heights.exe");
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Static Charts\bin\Debug\TeeGrid_Static_Charts.exe");
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Ticker\bin\Debug\TeeGrid_WinForm_Ticker.exe");
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\VirtualData\Array\bin\Debug\TeeGrid_Array_Data.exe");
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\VirtualData\Virtual Mode\bin\Debug\TeeGrid_VirtuaMode.exe");
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(folder + @"\Themes\bin\Debug\TeeGrid_Themes_WinForm.exe");
+		}
+	}
+}
