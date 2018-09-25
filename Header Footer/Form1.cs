908e4e929e7110b87f23f081cf00f61a92a8dd5c
Marc <marc@steema.com>
2018-09-25 11:24:53 +0200

    added column totals to demo

--- Header Footer/Form1.cs
+++ Header Footer/Form1.cs
@@ -12,6 +12,8 @@ using Steema.TeeGrid.Bands;
 using Steema.TeeGrid.Data.Reflection;
 using Steema.TeeGrid.Painter;
 using Steema.TeeGrid.WinForm.Editors;
+using Steema.TeeGrid.Totals;
+using Steema.TeeGrid.Data;
 
 namespace TeeGrid_Header_Footer
 {
@@ -30,15 +32,14 @@ namespace TeeGrid_Header_Footer
 			tmpFooter.Format.Font.Size = 14;
 			tmpFooter.Format.Font.Color = ColorTranslator.FromHtml("#3C95D5");
 
-
 			tTeeGrid1.Footer.AddText("Footer 2"+ Environment.NewLine + "SubFooter 2").Format.Font.Size = 14;
 			tTeeGrid1.Footer[1].Format.Font.Color = ColorTranslator.FromHtml("#014677");
 
 			tTeeGrid1.Footer.AddText("Footer 3").TextRender.TextAlign.Horizontal = HorizontalAlign.Center;
 			tTeeGrid1.Footer[2].Format.Font.Size = 25;
-		
+
 			//Customize Headers
-	
+
 			Steema.TeeGrid.Format.Brush brush = tTeeGrid1.Headers.AddText("Header 1").Format.Brush;
 			brush.Show();
 			brush.Gradient.Show();
@@ -58,6 +59,8 @@ namespace TeeGrid_Header_Footer
 			tTeeGrid1.Headers[3].Format.Stroke.Show();
 			tTeeGrid1.Headers[3].Format.Stroke.Size = 2;
 			tTeeGrid1.Headers[3].Format.Stroke.Color = ColorTranslator.FromHtml("#6BABD0");
+
+			//grid data
 			List <THouse> houses = new List<THouse>();
 			Random rnd = new Random();
 
@@ -71,6 +74,19 @@ namespace TeeGrid_Header_Footer
 			tmpBand.Index = 0;
 
 			tmpFooter.TextRender.TextAlign.Horizontal = HorizontalAlign.Center;
+
+			//standard footer
+			tTeeGrid1.Footer.AddText("My Footer");
+
+			//add column total title as footer
+			TotalsHeader theader = new TotalsHeader(tTeeGrid1.Footer);
+
+			//add column total footer
+			ColumnCalculation columnCalc = ColumnCalculation.Sum;
+
+			ColumnTotals cTotals = new ColumnTotals(tTeeGrid1.Footer); //if header required use tTeeGrid1.Headers as arg
+
+			cTotals.Calculation.Add(tTeeGrid1.Columns[1], columnCalc);
 
 		}
 
