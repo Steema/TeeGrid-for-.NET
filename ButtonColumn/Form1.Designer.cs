1f0dd3a499e18c31aa9c7ccd6dc87ca66d513919
Chris <cireland@gmail.com>
2020-01-13 12:22:13 +0100

    new ButtonColumn example

---
+++ ButtonColumn/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ButtonColumn
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tGrid1 = new Steema.TeeGrid.WinForm.TGrid();
+			this.SuspendLayout();
+			// 
+			// tGrid1
+			// 
+			this.tGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tGrid1.Location = new System.Drawing.Point(0, 0);
+			this.tGrid1.Name = "tGrid1";
+			this.tGrid1.Selected.ScrollToView = false;
+			this.tGrid1.Size = new System.Drawing.Size(701, 352);
+			this.tGrid1.TabIndex = 0;
+			this.tGrid1.Text = "tGrid1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(701, 352);
+			this.Controls.Add(this.tGrid1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+        #endregion
+
+        private Steema.TeeGrid.WinForm.TGrid tGrid1;
+    }
+}
+
---
+++ ButtonColumn/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Steema.TeeGrid.Columns;
+using Steema.TeeGrid.Data.Reflection;
+using Steema.TeeGrid.Painter;
+using Steema.TeeGrid.Renders;
+using Steema.TeeGrid.Selection;
+using Steema.TeeGrid.WinForm.Painter;
+
+namespace ButtonColumn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var myData = new MyButtonData[10];
+            var rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    myData[i] = new MyButtonData() { DummyData = "Five", ValueData = rnd.NextDouble() };
+                else
+                    myData[i] = new MyButtonData() { DummyData = "", ValueData = rnd.NextDouble() };
+            }
+
+            tGrid1.Data = new VirtualArrayData<MyButtonData>(myData);
+
+            tGrid1.Selected.Changed += Selected_Changed;
+
+            ChangeRender(tGrid1.Columns[0]);
+        }
+
+        private void Selected_Changed(object sender, EventArgs e)
+        {
+
+            var selection = sender as GridSelection;
+
+            if(selection.Column == tGrid1.Columns[0] && selection.Row != 5)
+            {
+                MessageBox.Show($"You've clicked a button on row {selection.Row}");
+            }
+        }
+
+        private void ChangeRender(Column column)
+        {
+            column.Render = new ButtonRender(column.Changed);
+        }
+    }
+
+
+    public class ButtonRender : TextRender
+    {
+        public ButtonRender(EventHandler<EventArgs> AChanged) : base(AChanged)
+        {
+        }
+
+        public override void Paint(RenderData AData)
+        {
+            void DoPaint(GdiPlusPainter painter, RectangleF bounds)
+            {
+                ControlPaint.DrawButton(painter.Canvas, Rectangle.Round(bounds), ButtonState.Normal);
+                painter.SetHorizontalAlign(HorizontalAlign.Center);
+                painter.TextOut(bounds, "Button Text");
+
+            }
+
+            if (string.IsNullOrEmpty(AData.Data))
+            {
+                DoPaint((GdiPlusPainter)AData.Painter, AData.Bounds);
+            }
+            else
+            {
+                base.Paint(AData);
+            }
+        }
+
+    }
+
+    public class MyButtonData
+    {
+        public string DummyData { get; set; }
+
+        public double ValueData { get; set; }
+    }
+}
