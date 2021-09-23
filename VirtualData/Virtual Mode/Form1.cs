38fa26601984b19c6cfda5cf2a5c11c7157bb2d0
Marc <marc@steema.com>
2021-09-23 12:14:23 +0200

    modification to set cell color and font color/size

--- VirtualData/Virtual Mode/Form1.cs
+++ VirtualData/Virtual Mode/Form1.cs
@@ -37,9 +37,52 @@ namespace TeeGrid_VirtuaMode
 
 			teeGrid1.Data = data;
 
-		}
+            teeGrid1.Columns[4].OnPaint += Column_OnPaint;
 
-		private void SetCell(object sender, VirtualDataEventArgs e)
+           // teeGrid1.Columns[7].OnPaint += Column_OnPaint;
+
+
+        }
+
+        private static EventHandler<EventArgs> eventType;
+
+        Steema.TeeGrid.Format.Format customFormat = new Steema.TeeGrid.Format.Format(eventType);
+        Steema.TeeGrid.Format.TextFormat custTextFormat = new Steema.TeeGrid.Format.TextFormat(eventType);
+
+        private void Column_OnPaint(object sender, Steema.TeeGrid.Columns.ColumnPaintEventArgs e)
+        {
+            if (e.AData.Row == 3)
+            {
+                customFormat.Brush.Color = Color.Beige;
+                e.AData.Painter.Paint(customFormat, e.AData.Bounds);
+
+                //no text
+            }
+            else if (e.AData.Row == 7)
+            {
+                Steema.TeeGrid.Format.Font oldFont = ((Steema.TeeGrid.Columns.Column)(sender)).Format.Font;
+
+                customFormat.Stroke.Color = Color.Red;
+                e.AData.Painter.Paint(customFormat, e.AData.Bounds); //brush already set
+
+                Steema.TeeGrid.Format.Font customFont = new Steema.TeeGrid.Format.Font(eventType);
+
+                customFont.Color = Color.Blue;
+                customFont.Size = 15;
+
+                custTextFormat.Font = customFont;
+
+                e.AData.Painter.SetFont(customFont);
+
+                ((Steema.TeeGrid.Renders.Render)((Steema.TeeGrid.Columns.Column)(sender)).Render).Paint(e.AData);
+
+                e.AData.Painter.SetFont(oldFont);
+            }
+            else
+                ((Steema.TeeGrid.Renders.Render)((Steema.TeeGrid.Columns.Column)(sender)).Render).Paint(e.AData);
+        }
+
+        private void SetCell(object sender, VirtualDataEventArgs e)
 		{
 			// Called when the cell has been manually edited, or when a Data cell is changed.
 			// Store the new e.Value in your real data
